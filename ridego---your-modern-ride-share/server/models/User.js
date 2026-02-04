960b100ac201ce4d4aa123f18f1375a4973d4a70
Seventie <abdussattarshaik07@gmail.com>
2026-02-04 09:37:55 +0530

    Adding a new schema

--- ridego---your-modern-ride-share/server/models/User.js
+++ ridego---your-modern-ride-share/server/models/User.js
@@ -13,4 +13,16 @@ const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },
 });
 
+const dummySchema = new mongoose.Schema({
+    role: { type: String, enum: ['RIDER', 'DRIVER'], required: true },
+    phone: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: String, required: true },
+    gender: { type: String, required: true },
+    // Driver specific fields
+    license: { type: String },
+    aadhar: { type: String },
+    createdAt: { type: Date, default: Date.now },
+});
 module.exports = mongoose.model('User', userSchema);
